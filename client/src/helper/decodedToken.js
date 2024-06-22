68ad68e081a72c0b5d26ebf582fe6efd26dbbcee
OMKAR <omkaranbhule12@gmail.com>
2024-06-22 21:21:03 +0530

    update

--- client/src/helper/decodedToken.js
+++
@@ -1,9 +0,0 @@
-import React, { useState, useEffect } from 'react';
-import { jwtDecode } from 'jwt-decode';
-
-
-const decodedToken = () => {
-    return jwtDecode(localStorage.getItem('token'));
-};
-
-export default decodedToken
